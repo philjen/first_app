6aa08a8585f68e2d120ae8b822d0a6e15946bd19
Phil <phil@crowdtilt.com>
2013-08-14 14:11:57 -0700

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'accce4e0644b4836bfbcade45f064bee9a5e3cbb16107bb1c922124dacfb17afadb550cb2eafeee9e8d06fa9d77b93e24b4e57f5620dc785c0a4a2756d22ca57'
